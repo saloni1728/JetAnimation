6aa1e5af036a09d803017133427b192fd95c8c9e
JainSaloni4 <saloni.jain@housing.com>
2025-08-04 23:02:58 +0530

    unnecessary code removal

--- app/src/main/java/com/example/invest/MainActivity.kt
+++ app/src/main/java/com/example/invest/MainActivity.kt
@@ -51,19 +51,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InvestTheme {
-        Greeting("Android")
-    }
-}
